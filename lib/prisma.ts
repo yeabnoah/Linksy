8d7b22d7656f40799473a353ce8eed433b2985ee
yeabnoah <yeabnoah5@gmail.com>
2024-12-31 20:34:58 +0300

    fixing framer issues for vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
 };
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
 
 export default prisma;
