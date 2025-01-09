a2c114b0f1fae60a21577893c05c658e861bdd64
yeabnoah <yeabnoah5@gmail.com>
2025-01-09 22:43:13 +0300

    final

--- util/getUserData.ts
+++ util/getUserData.ts
@@ -1,6 +1,7 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 
+// a function to get users session and authenticates the user -----
 const getUserSession = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
